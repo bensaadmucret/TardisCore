b0dcbbb6e2b892a0ae35a0c362636e1de164227a
Ben <ben@aixmarketing4-BIS.local>
2021-03-04 21:22:08 +0100

    --- Auto-Git Commit ---

--- src/Tardis/Router/RouterInterface.php
+++ src/Tardis/Router/RouterInterface.php
@@ -6,5 +6,21 @@ namespace Tardis\Router;
 
 interface RouterInterface
 {
+    /**
+     * J'ajoute une route a la table des routes.
+     *
+     * @param string $route
+     * @param array $params
+     * @return void
+     */
     public function add(string $route, array $params) : void;
+
+    /**
+     *  dispatcher une tâche définie par certaines données sur 
+     * un contrôleur et une action en fonction de la nature des données manipulées
+     *
+     * @param string $url
+     * @return void
+     */
+    public function dispatch(string $url) : void; 
 }
