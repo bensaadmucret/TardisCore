c58f8f6eccd503218c6089155bd444537744402c
mohammedBensaad <bensaadmucret@gmail.com>
2021-03-15 19:58:25 +0100

    phpunit.xml

---
+++ src/Tardis/Router/Route.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tardis\Router;
+
+class Route
+{
+}
---
+++ src/Tardis/Tests/RouterTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tardis\Tests;
+
+use Tardis\Router\Router;
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase
+{
+    /**
+     * Test router
+     * @return void
+     */
+    public function test()
+    {
+        $router = new Router();
+        $this->assertTrue(true);
+    }
+}
